c55773b9a852a5e6d3f596523e563b350f82492a
yasu37 <kanyasuhiro34@gmail.com>
2021-04-04 10:49:00 +0900

    third add file

---
+++ test3.php
@@ -0,0 +1,27 @@
+<?php
+mb_language('japanese');
+mb_internal_encoding('UTF-8');
+
+$to = $_POST['to'];
+$subject = $_POST['title'];
+$message = $_POST['content'];
+$headers = "from@example.com";
+
+?>
+
+<!DOCTYPE html>
+<html lang="ja">
+
+<head>
+  <meta charset="utf-8" />
+</head>
+<body>
+<?php
+if(mb_send_mail($to, $subject, $message, $headers)) {
+	echo "メール送信成功です";
+} else {
+	echo "メール送信失敗です";
+}
+?>
+</body>
+</html>
