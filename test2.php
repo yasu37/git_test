95d532c61bbe7fafb78fc678b5ffd89d9526cbcc
yasu37 <kanyasuhiro34@gmail.com>
2021-04-04 11:29:32 +0900

    add title to test2.php

--- test2.php
+++ test2.php
@@ -2,14 +2,15 @@
 <html lang="ja">
 
 <head>
-  <meta charset="utf-8" />
+<meta charset="utf-8" />
+<title>メール送信フォーム</title>
 </head>
 <body>
-  <form action="./test_mail.php" method="post">
-    <p>送り先</p><input type="text" name="to">
-    <p>件名</p><input type="text" name="title">
-    <p>メッセージ</p><textarea name="content" cols="60" rows="10"></textarea>
-    <p><input type="submit" name="send" value="送信"></p>
-  </form>
+<form action="./test_mail.php" method="post">
+<p>送り先</p><input type="text" name="to">
+<p>件名</p><input type="text" name="title">
+<p>メッセージ</p><textarea name="content" cols="60" rows="10"></textarea>
+<p><input type="submit" name="send" value="送信"></p>
+</form>
 </body>
 </html>
